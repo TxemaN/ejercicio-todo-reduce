abbdb809212ea12464cbcc1ddf5674955df32b2e
carlOzil <seifer01es@msn.com>
2023-09-18 14:08:30 +0200

    correcciones clase

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { useTodoList } from '../hooks/useTodoList';
 
-export const Tarea = ({ tarea, deleteTodo, toggleTodo }) => {
+export const Tarea = () => {
+
+    const { todos, handleDeleteTodo, handleToggleTodo } = useTodoList();
+
     return (
         <div className='row border'>
             <div className='col col-8'>
-                <p className={tarea.done?'text-body-secondary text-decoration-line-through':''}><strong>{tarea.todo}</strong> - {tarea.description}</p>
+                <p className={todos.done ? 'text-body-secondary text-decoration-line-through' : ''}><strong>{todos.todo}</strong> - {todos.description}</p>
             </div>
             <div className='col col-4 text-end'>
-                <button className={tarea.done ? 'btn btn-dark' : 'btn btn-success'} onClick={() => toggleTodo(tarea.id)}>
-                {tarea.done ? 'Finalizada' : 'Pendiente'}
-            </button>
-                <button className='btn btn-danger m-1' onClick={() => deleteTodo(tarea.id)}>Eliminar</button>
+                <button className={todos.done ? 'btn btn-dark' : 'btn btn-success'} onClick={() => handleToggleTodo(todos.id)}>
+                    {todos.done ? 'Finalizada' : 'Pendiente'}
+                </button>
+                <button className='btn btn-danger m-1' onClick={() => handleDeleteTodo(todos.id)}>Eliminar</button>
             </div>
         </div>
     );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Tarea } from './Tarea';
 
-export const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
+export const TodoList = ({ todos}) => {
     return (
         <section className='container'>
             <h3>Tareas</h3>
             {
-                todos.map(tarea => (
-                    <Tarea
-                        key={tarea.id}
-                        tarea={tarea}
-                        deleteTodo={deleteTodo}
-                        toggleTodo={toggleTodo}
-                    />
+                todos.map(todo => (
+                    <Tarea key={todo.id}/>
                 ))
             }
         </section>
